e58966fe25dab2797b47a1d17b6c301184e0826c
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-12-22 10:21:49 -0700

    added font-awesome

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const dateOfEvent = document.querySelectorAll('.event-date');
-const completedEvent = document.querySelectorAll('.event');
+const dateOfEvent = document.querySelectorAll(".event-date");
+// const completedEvent = document.querySelectorAll('.event');
 
-dateOfEvent.forEach(event => {
-    const date = new Date(event.dateTime);
-    // console.log(date);
-    const currentDate = new Date();
-    if (date < currentDate) {
-        event.style.color = '#e51212';
-        event.style.textDecoration = 'line-through';
-        // hide elements
-        // event.style.display = 'none';
-    }
-})
+dateOfEvent.forEach((event) => {
+  const date = new Date(event.dateTime);
+  // console.log(date);
+  const currentDate = new Date();
+  if (date < currentDate) {
+    console.log(event);
+    // event.style.color = "#e51212";
+    event.style.textDecoration = "line-through";
+    // hide elements
+    // event.style.display = 'none';
+  }
+});
