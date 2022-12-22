1f3faea8327fa3fdf56035f99a49319efd4c90f7
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2022-12-22 09:18:27 -0700

    more styling

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const dateOfEvent = document.querySelectorAll('.event-date');
+const completedEvent = document.querySelectorAll('.event');
 
 dateOfEvent.forEach(event => {
     const date = new Date(event.dateTime);
     // console.log(date);
     const currentDate = new Date();
     if (date < currentDate) {
+        event.style.color = '#e51212';
         event.style.textDecoration = 'line-through';
+        // hide elements
+        // event.style.display = 'none';
     }
 })
